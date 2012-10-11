230e3f07e840ff137c9045861ff0412ad15db42e
serialk <antoine.pietri@epita.fr>
2012-10-11 19:42:16 +0200

    matrix.ml,image.ml : added some usual functions

--- image/image.ml
+++ image/image.ml
@@ -2,11 +2,17 @@ let bool2color = function
     | true -> Sdlvideo.black
     | false -> Sdlvideo.white
 
+let get_dims img = (
+      (Sdlvideo.surface_info img).Sdlvideo.w,
+      (Sdlvideo.surface_info img).Sdlvideo.h
+)
+
+let create_surface w h = Sdlvideo.create_RGB_surface [] ~w:w ~h:h ~bpp:8
+    ~rmask:Int32.zero ~gmask:Int32.zero ~bmask:Int32.zero ~amask:Int32.zero 
+
 let matrix_to_surface matrix =
     let h, w = Matrix.nbcols matrix, Matrix.nbrows matrix in
-    let z = Int32.zero in
-    let surface  = Sdlvideo.create_RGB_surface []
-        ~w:w ~h:h ~bpp:8 ~rmask:z ~gmask:z ~bmask:z ~amask:z in
+    let surface = create_surface w h in
     begin
         for c = 0 to w - 1 do
             for r = 0 to h - 1 do
--- matrix.ml
+++ matrix.ml
@@ -3,3 +3,11 @@ let nbcols x = Array.length x.(0)
 let make = Array.make_matrix
 let coords (c,r) h = (c, h - r - 1)
 let pos (x,y) h = (h - y - 1, x)
+let print_matrix mat =
+    for r = 0 to Matrix.nbrows mat - 1 do
+        for c = 0 to Matrix.nbcols mat - 1 do
+            Printf.printf "%B " mat.(r).(c);
+        done;
+        Printf.printf "\n";
+    done
+
