6e9cf7ccab54089115fe526df22b0b04f9d6cad1
serialk <antoine.pietri@epita.fr>
2012-10-11 19:50:05 +0200

    added int_of_bool.

--- ocr/matrix.ml
+++ ocr/matrix.ml
@@ -3,10 +3,12 @@ let nbcols x = Array.length x.(0)
 let make = Array.make_matrix
 let coords (c,r) h = (c, h - r - 1)
 let pos (x,y) h = (h - y - 1, x)
+let int_of_bool (b: bool) = if b then 1 else 0;;
+
 let print_matrix mat =
-    for r = 0 to Matrix.nbrows mat - 1 do
-        for c = 0 to Matrix.nbcols mat - 1 do
-            Printf.printf "%B " mat.(r).(c);
+    for r = 0 to (nbrows mat) - 1 do
+        for c = 0 to (nbcols mat) - 1 do
+            Printf.printf "%d " (int_of_bool mat.(r).(c));
         done;
         Printf.printf "\n";
     done
