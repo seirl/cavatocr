cd35f2e3cb0242293c2e83b2627aa2dc450173d8
serialk <antoine.pietri@epita.fr>
2012-10-11 02:28:46 +0200

    Attempt to fix rotate

--- processing/rotate.ml
+++ processing/rotate.ml
@@ -1,10 +1,12 @@
+let pi = 3.14159265
+
 let cartesian_to_polar (x,y) = 
     let (x,y) = (float x, float y) in
     (sqrt (x**2. +. y**2.), atan2 y x)
 
 let polar_to_cartesian (r,t) =
     let (x,y) = (r *. cos t, r *. sin t) in
-    (int_of_float x, int_of_float y)
+    (int_of_float x, int_of_float y + 1)
 
 let rotate_polar (r,t) angle =
     (r, t +. angle)
@@ -15,7 +17,15 @@ let rotate (x,y) angle =
 let newdims w h angle =
     let wx, wy = rotate (w,0) angle in
     let hx, hy = rotate (0,h) angle in
-    (max wx hx, max wy hy)
+    ((max (abs wx) (abs hx)) + 2, (max (abs wy) (abs hy)) + 2)
+
+let print_matrix mat =
+    for r = 0 to Matrix.nbrows mat - 1 do
+        for c = 0 to Matrix.nbcols mat - 1 do
+            Printf.printf "%B " mat.(r).(c);
+        done;
+        Printf.printf "\n";
+    done
 
 let rotate_matrix src angle =
     let (ow, oh) = (Matrix.nbcols src, Matrix.nbrows src) in
@@ -25,10 +35,24 @@ let rotate_matrix src angle =
         for r = 0 to ow - 1 do
             for c = 0 to oh - 1 do
                 let (x, y) = Matrix.coords (c,r) oh in
+                let (x, y) = (x - ow / 2, y - oh / 2) in
                 let (nx, ny) = rotate (x, y) angle in
-                let (nc, nr) = Matrix.pos (nx, ny) nh in
+                let (nc, nr) = Matrix.pos (nx + nw / 2, ny + nh / 2) nh in
+                Printf.printf "%d %d\n" r c;
+                Printf.printf "%d %d\n" nr nc;
+                print_matrix dst;
                 dst.(nr).(nc) <- src.(r).(c)
             done
         done;
     dst
     end
+
+let _ =
+    let mat = [| [| false ; false ; true |];
+        [| true ; false ; true |];
+        [| false ; false ; true |];
+        [| false ; true ; true |];
+    |] in 
+    print_matrix (rotate_matrix mat (pi *. 2.));
+    let x,y = rotate (5,5) (pi *. 2.) in
+    Printf.printf "%d %d" x y
