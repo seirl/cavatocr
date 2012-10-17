371abed875f3bc6dd146d77a6f2891e7b35be7f5
serialk <antoine.pietri@epita.fr>
2012-10-17 13:37:25 +0200

    added some comments

--- ocr/processing/rotate.ml
+++ ocr/processing/rotate.ml
@@ -1,5 +1,6 @@
 let pi = 3.14159
 
+(* Rotate the image matrix by the specified angle *)
 let rotate mat angle =
     let (cos_a, sin_a) = (cos angle, sin angle) in
     let (w, h) = Matrix.get_dims mat in
@@ -26,7 +27,9 @@ let rotate mat angle =
     mat2
 
 
+(* Convert a radian angle into a degree angle *)
 let to_degrees = function rad -> (rad *. 180.) /. pi
+(* Convert a degree angle into a radian angle *)
 let to_radians = function deg -> (deg *. pi) /. 180.
 
 
@@ -34,7 +37,7 @@ let histogram mat angle =
     let (w,h) = Matrix.get_dims mat in
     angle
 
-
+(* Get the skew angle of the image matrix *)
 let get_skew_angle mat =
     let hist_opt = ref 0.
     and angle_opt = ref 0. in
