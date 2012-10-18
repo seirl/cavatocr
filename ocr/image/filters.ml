08056f0cc8e7457b78cdcda068b1bf68bcbacb41
Paul Hervot <p_hervot@hotmail.fr>
2012-10-18 23:37:16 +0200

    filters: add laplacian edge detection

--- ocr/image/filters.ml
+++ ocr/image/filters.ml
@@ -46,6 +46,67 @@ let binarize imageBw =
       imageBin
     end
 
+(** Laplacian edge detection finalisation *)
+let laplacian_end imageBin = 
+        let (w,h)= Image.get_dims imageBin in
+        for x = 0 to w - 1 do 
+               if g (Sdlvideo.get_pixel_color imageBin x 0) < 127 then
+                     Sdlvideo.put_pixel_color imageBin x 0 (0,0,0)
+               else Sdlvideo.put_pixel_color imageBin x 0 (255,255,255);
+
+               if g (Sdlvideo.get_pixel_color imageBin x (h-1)) < 127 then
+                     Sdlvideo.put_pixel_color imageBin x (h-1)  (0,0,0)
+               else Sdlvideo.put_pixel_color imageBin x (h-1) (255,255,255)
+        done;
+        
+        for y = 0 to h -1 do
+               if g (Sdlvideo.get_pixel_color imageBin 0 y ) < 127 then
+                       Sdlvideo.put_pixel_color imageBin 0 y (0,0,0)
+               else Sdlvideo.put_pixel_color imageBin 0 y (255,255,255);
+
+               if g (Sdlvideo.get_pixel_color imageBin (w-1) y ) < 127 then
+                     Sdlvideo.put_pixel_color imageBin (w-1) y (0,0,0)
+               else Sdlvideo.put_pixel_color imageBin (w-1) y
+                      (255,255,255) 
+        done
+
+(** Laplacian edge detection *)
+let laplacian imageBw =
+  let(w,h) =Image.get_dims imageBw in
+  let imageBin = Image.create_surface w h in
+    begin
+      for y = 1 to h - 2 do
+        for x = 1 to w - 2 do
+          if
+            (
+                g(Sdlvideo.get_pixel_color imageBw (x-1) (y-1))
+                +
+                g(Sdlvideo.get_pixel_color imageBw  x (y-1))
+                +
+                g(Sdlvideo.get_pixel_color imageBw (x+1) (y-1))
+                +
+                g(Sdlvideo.get_pixel_color imageBw (x-1) (y))
+                +
+                g(Sdlvideo.get_pixel_color imageBw (x+1) (y))
+                +
+                g(Sdlvideo.get_pixel_color imageBw (x-1) (y+1))
+                +
+                g(Sdlvideo.get_pixel_color imageBw (x) (y+1))
+                +
+                g(Sdlvideo.get_pixel_color imageBw (x+1) (y+1))
+            )/8
+          < (* Less than *)
+            g(Sdlvideo.get_pixel_color imageBw x y )
+          then
+            Sdlvideo.put_pixel_color imageBin x y (0,0,0)
+          else
+            Sdlvideo.put_pixel_color imageBin x y (255,255,255)
+        done;
+      done;
+      laplacian_end imageBin;
+      imageBin
+    end
+
 let clean_bin img = img 
 
 let rec mathmedian l length = match l with
