4cd299f3ac37ed14603a331df4645c7a3d12175d
serialk <antoine.pietri@epita.fr>
2012-10-18 18:02:06 +0200

    new main.ml

--- ocr/main.ml
+++ ocr/main.ml
@@ -2,8 +2,8 @@ let _ =
     Image.sdl_init ();
     let surface = Image.load Sys.argv.(1) in
     let display = Image.display_for_image surface in
-    let mat = (Preprocessing.binarise surface (Preprocessing.seuil surface)) in
-    let rotate_mat = Rotate.rotate (Preprocessing.sorttable mat) 0.2 in
+    let mat = (Filters.binarise surface (Filters.seuil surface)) in
+    let rotate_mat = Rotate.rotate (Filters.sorttable mat) 0. in
     Image.show surface display;
     Image.wait_key();
     let surface2 = Image.surface_of_matrix rotate_mat in
