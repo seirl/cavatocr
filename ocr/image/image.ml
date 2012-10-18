06a464b32d57ad675cca3a512055f46a3a0e32e7
serialk <antoine.pietri@epita.fr>
2012-10-18 16:21:20 +0200

    multiple fixes in preprocessing.ml

--- ocr/image/image.ml
+++ ocr/image/image.ml
@@ -1,34 +1,35 @@
-(* Get the dimensions of an SDL surface: a couple (witdh, height) *)
+(** Get the dimensions of an SDL surface: a couple (witdh, height) *)
 let get_dims img =
 (
       (Sdlvideo.surface_info img).Sdlvideo.w,
       (Sdlvideo.surface_info img).Sdlvideo.h
 )
 
-(* Initialize SDL *)
+(** Initialize SDL *)
 let sdl_init () =
   begin
     Sdl.init [`EVERYTHING];
     Sdlevent.enable_events Sdlevent.all_events_mask;
   end
 
-(* Load an image as a surface *)
+(** Load an image as a surface *)
 let load = Sdlloader.load_image
 
-(* Get a display surface *)
+(** Get a display surface *)
 let display w h = Sdlvideo.set_video_mode w h [`DOUBLEBUF]
-(* Same with the size of a given surface *)
+
+(** Same with the size of a given surface *)
 let display_for_image img =
   let w, h = get_dims img in
     display w h
 
-(* Display an SDL surface on the dst display *)
+(** Display an SDL surface on the dst display *)
 let show img dst =
   let d = Sdlvideo.display_format img in
     Sdlvideo.blit_surface d dst ();
     Sdlvideo.flip dst
 
-(* Wait for a key to be pressed before continue *)
+(** Wait for a key to be pressed before continue *)
 let rec wait_key () =
   let e = Sdlevent.wait_event () in
     match e with
@@ -39,11 +40,11 @@ let color_of_bool = function
     | true -> Sdlvideo.black
     | false -> Sdlvideo.white
 
-(* Initialize an SDL surface *)
+(** Initialize an SDL surface *)
 let create_surface w h = Sdlvideo.create_RGB_surface [] ~w:w ~h:h ~bpp:8
     ~rmask:Int32.zero ~gmask:Int32.zero ~bmask:Int32.zero ~amask:Int32.zero 
 
-(* Convert a bool matrix (array array) to a black/white SDL surface *)
+(** Convert a bool matrix (array array) to a black/white SDL surface *)
 let surface_of_matrix matrix =
     let w, h = Matrix.nbcols matrix, Matrix.nbrows matrix in
     let surface = create_surface w h in
--- ocr/image/preprocessing.ml
+++ ocr/image/preprocessing.ml
@@ -1,62 +1,69 @@
-(* met mon image enoir et blanc *)
-let level (r,g,b) = ( float_of_int(r) *. 0.3 +. float_of_int(g) *.  0.59 +.
-                      float_of_int(b) *. 0.11) /. 255.
+(** Get the gray level of a color *)
+let level (r,g,b) =
+    (float_of_int(r) *. 0.3 +.
+     float_of_int(g) *.  0.59 +.
+     float_of_int(b) *. 0.11) /. 255.
 
-let color2grey (r,g,b) = let grey = int_of_float (level (r,g,b) *. 255.) in (grey,grey,grey)
+(** Recreates the color from its grey level *)
+let color2grey (r,g,b) =
+    let grey = int_of_float (level (r,g,b) *. 255.)
+    in (grey,grey,grey)
 
+(** Turns an image into greyscale *)
 let image2grey src dst =
-  let (w,h) = Image.get_dims src in
+    let (w,h) = Image.get_dims src in
     for y = 0 to h - 1 do
-      for x = 0 to w - 1 do
-        Sdlvideo.put_pixel_color dst x y (color2grey (Sdlvideo.get_pixel_color src x y ))
-      done
+        for x = 0 to w - 1 do
+            Sdlvideo.put_pixel_color dst x y
+        (color2grey (Sdlvideo.get_pixel_color src x y ))
+        done
     done
 
-
-(* fixage du seuil de binarisation  avec un s definie avant (je sais c est
- * deguelasse)
- *)
-let seuil imageBW s =
-  let (w,h) = Image.get_dims imageBW in
+(** Binarization step *)
+let seuil imageBW =
+    let s = ref 0 in
+    let (w,h) = Image.get_dims imageBW in
     for y = 0 to h - 1 do
-      for x = 0 to w - 1 do
-        let (a,_,_)= Sdlvideo.get_pixel_color imageBW x y in
-          s:=!s+a
-      done
-    done ;
-    s:= !s /(w*h);
-    !s
+        for x = 0 to w - 1 do
+            let (a,_,_) = Sdlvideo.get_pixel_color imageBW x y in
+            s := !s+a
+        done
+    done;
+    !s / (w*h);
 
 
-(* binarisation *)
-let transform (x,y,z) tolerance = match (x,y,z)with
-  |(x,y,z) when x < tolerance -> false
-  |(x,y,z) -> true
+(** Pixel binarization *)
+let transform (x,y,z) tolerance = match (x,y,z) with
+    | (x,y,z) when x < tolerance -> false
+    | (x,y,z) -> true
 
+(** Image binarization *)
 let binarise imageBw tolerance =
-  let (w,h) = Image.get_dims imageBw in
-  let arr = Array.make_matrix w h false in
+    let (w,h) = Image.get_dims imageBw in
+    let arr = Array.make_matrix w h false in
     for y = 0 to h - 1 do
-      for x =0 to w - 1 do
-        arr.(y).(x) <- (transform (Sdlvideo.get_pixel_color imageBw x y) tolerance)
-      done;
+        for x = 0 to w - 1 do
+            arr.(x).(y) <-
+                (transform (Sdlvideo.get_pixel_color imageBw x y) tolerance)
+        done;
     done;
     arr
 
 
-(* suppression de bruit *)
+(** Delete noize *)
 let sorttable arraytable =
-  for i = 0 to Array.length arraytable - 1 do
-    let minplace = ref i
-    and temp = arraytable.(i) in
-      for j = i to Array.length (arraytable.(i)) - 1 do
-        if arraytable.(j) < arraytable.(!minplace) then
-          minplace := j
-      done ;
-      arraytable.(i) <- arraytable.(!minplace) ;
-      arraytable.(!minplace) <- temp
-  done
+    for i = 0 to Array.length arraytable - 1 do
+        let minplace = ref i
+        and temp = arraytable.(i) in
+        for j = i to Array.length (arraytable.(i)) - 1 do
+            if arraytable.(j) < arraytable.(!minplace) then
+                minplace := j
+        done;
+        arraytable.(i) <- arraytable.(!minplace) ;
+        arraytable.(!minplace) <- temp
+    done
 
 let rec mathmedian l length = match l with
-  |e::l when length = 5 -> e
-  |_::l -> mathmedian l (length-1)
+    | [] -> failwith "wat"
+    | e::l when length = 5 -> e
+    | _::l -> mathmedian l (length-1)
