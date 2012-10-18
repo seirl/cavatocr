f72539839699f5e21b5a44a653adfeabaeb59839
serialk <antoine.pietri@epita.fr>
2012-10-18 17:40:38 +0200

    added main.ml with a basic binarization and rotation

---
+++ ocr/main.ml
@@ -0,0 +1,13 @@
+let _ =
+    Image.sdl_init ();
+    let surface = Image.load Sys.argv.(1) in
+    let display = Image.display_for_image surface in
+    let mat = (Preprocessing.binarise surface (Preprocessing.seuil surface)) in
+    let rotate_mat = Rotate.rotate mat 0.2 in
+    Image.show surface display;
+    Image.wait_key();
+    let surface2 = Image.surface_of_matrix rotate_mat in
+    let display2 = Image.display_for_image surface2 in
+    Image.show surface2 display2;
+    Image.wait_key();
+    exit 0
