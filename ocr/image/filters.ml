a350c0436998a6215035a81fb16a66ea88123996
Paul Hervot <p_hervot@hotmail.fr>
2012-10-18 23:28:49 +0200

    filters: simple binarization

--- ocr/image/filters.ml
+++ ocr/image/filters.ml
@@ -31,91 +31,22 @@ let transform (x,y,z) tolerance = match (x,y,z) with
     | (x,y,z) -> true
 
 (** Image binarization *)
-let binarise imageBw tolerance =
-    let (w,h) = Image.get_dims imageBw in
-    let arr = Array.make_matrix w h false in
-    for y = 0 to h - 1 do
-        for x = 0 to w - 1 do
-            arr.(x).(y) <-
-                (transform (Sdlvideo.get_pixel_color imageBw x y) tolerance)
-        done;
-    done;
-    arr
-
-(**Image binarization borders pixels *)
-let binariseDoctor imageBin = 
-        let (w,h)= Image.get_dims imageBin in
-        for x = 0 to w - 1 do 
-               if g (Sdlvideo.get_pixel_color imageBin x 0) < 127 then
-                     Sdlvideo.put_pixel_color imageBin x 0 (0,0,0)
-               else Sdlvideo.put_pixel_color imageBin x 0 (255,255,255);
-
-               if g (Sdlvideo.get_pixel_color imageBin x (h-1)) < 127 then
-                     Sdlvideo.put_pixel_color imageBin x (h-1)  (0,0,0)
-               else Sdlvideo.put_pixel_color imageBin x (h-1) (255,255,255)
-        done;
-        
-        for y = 0 to h -1 do
-               if g (Sdlvideo.get_pixel_color imageBin 0 y ) < 127 then
-                       Sdlvideo.put_pixel_color imageBin 0 y (0,0,0)
-               else Sdlvideo.put_pixel_color imageBin 0 y (255,255,255);
-
-               if g (Sdlvideo.get_pixel_color imageBin (w-1) y ) < 127 then
-                     Sdlvideo.put_pixel_color imageBin (w-1) y (0,0,0)
-               else Sdlvideo.put_pixel_color imageBin (w-1) y
-                      (255,255,255) 
-        done
-
-(**image binarization all expect borders pixels*)
-let binariseV2 imageBw =
+let binarize imageBw =
   let(w,h) =Image.get_dims imageBw in
   let imageBin = Image.create_surface w h in
     begin
-      for y = 1 to h - 2 do
-        for x = 1 to w - 2 do
-          if
-            (
-                g(Sdlvideo.get_pixel_color imageBw (x-1) (y-1))
-                +
-                g(Sdlvideo.get_pixel_color imageBw  x (y-1))
-                +
-                g(Sdlvideo.get_pixel_color imageBw (x+1) (y-1))
-                +
-                g(Sdlvideo.get_pixel_color imageBw (x-1) (y))
-                +
-                g(Sdlvideo.get_pixel_color imageBw (x+1) (y))
-                +
-                g(Sdlvideo.get_pixel_color imageBw (x-1) (y+1))
-                +
-                g(Sdlvideo.get_pixel_color imageBw (x) (y+1))
-                +
-                g(Sdlvideo.get_pixel_color imageBw (x+1) (y+1))
-            )/8
-          < (* Less than *)
-            g(Sdlvideo.get_pixel_color imageBw y x )
-          then
+      for y = 0 to h do
+        for x = 0 to w do
+          if g(Sdlvideo.get_pixel_color imageBw x y) < 180 then
             Sdlvideo.put_pixel_color imageBin x y (0,0,0)
           else
             Sdlvideo.put_pixel_color imageBin x y (255,255,255)
         done;
       done;
-      binariseDoctor imageBin;
       imageBin
     end
 
-(** Delete noize *)
-let sorttable arraytable =
-    for i = 0 to Array.length arraytable - 1 do
-        let minplace = ref i
-        and temp = arraytable.(i) in
-        for j = i to Array.length (arraytable.(i)) - 1 do
-            if arraytable.(j) < arraytable.(!minplace) then
-                minplace := j
-        done;
-        arraytable.(i) <- arraytable.(!minplace) ;
-        arraytable.(!minplace) <- temp
-    done;
-    arraytable
+let clean_bin img = img 
 
 let rec mathmedian l length = match l with
     | [] -> failwith "wat"
