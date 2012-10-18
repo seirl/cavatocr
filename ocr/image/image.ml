1463d24c607637965c3823fefe967c13d56e6605
Paul Hervot <p_hervot@hotmail.fr>
2012-10-18 03:15:28 +0200

    image.ml: delet uncomplet function

--- ocr/image/image.ml
+++ ocr/image/image.ml
@@ -11,7 +11,7 @@ let create_surface w h = Sdlvideo.create_RGB_surface [] ~w:w ~h:h ~bpp:8
     ~rmask:Int32.zero ~gmask:Int32.zero ~bmask:Int32.zero ~amask:Int32.zero 
 
 let matrix_to_surface matrix =
-    let h, w = Matrix.nbcols matrix, Matrix.nbrows matrix in
+    let w, h = Matrix.nbcols matrix, Matrix.nbrows matrix in
     let surface = create_surface w h in
     begin
         for c = 0 to w - 1 do
@@ -22,10 +22,3 @@ let matrix_to_surface matrix =
         done;
         surface
     end
-
-let surface_to_matrix surface =
-    
-
-
-    
-
