4e825ba5b838f227c2aec66ce696731cd8a4d2f7
Paul Hervot <p_hervot@hotmail.fr>
2012-10-21 17:30:52 +0200

    blocks: indentation

--- ocr/processing/blocks.ml
+++ ocr/processing/blocks.ml
@@ -23,23 +23,25 @@ let get_lines (img:bool array array) =
   let h = Array.length hist in
   let threshold = 2 in
   let rec merge_per_line (y:int) (accu:bool array array list) =
-      (* End of image, return final merge *)
-      if y >= h then accu
+    (* End of image, return final merge *)
+    if y >= h then accu
 
-      else let line = get_line y h img in
-        (* Empty line, discard and evaluate next *)
-        if hist.(y) <= threshold then merge_per_line (y + 1) accu
+    else let line = get_line y h img in
+      (* Empty line, discard and evaluate next *)
+      if hist.(y) <= threshold then merge_per_line (y + 1) accu
 
-        (* First line and non-empty or new non-empty line after some empty *)
-        else if y = 0 || hist.(y - 1) <= threshold
-        then merge_per_line (y + 1) ([| line |] :: accu)
+      (* First line and non-empty or new non-empty line after some empty *)
+      else if y = 0 || hist.(y - 1) <= threshold
+      then merge_per_line (y + 1) ([| line |] :: accu)
 
-        (* Another non-empty line *)
-        else match accu with
-          | [] -> failwith
-                    "merge_line: not first non-empty line but accu is empty"
-          | block_lines :: accu_t ->
-              merge_per_line (y + 1)
-                ((Array.append block_lines ([| line |])) :: accu_t)
+      (* Another non-empty line *)
+      else match accu with
+        | [] -> failwith
+                  "merge_line: not first non-empty line but accu is empty"
+        | block_lines :: accu_t ->
+            merge_per_line (y + 1)
+              ((Array.append block_lines ([| line |])) :: accu_t)
   in
-    List.map (fun mat -> Rotate.rotate mat (Rotate.pi /. (-2.))) (merge_per_line 0 [])
+    List.map
+      (fun mat -> Rotate.rotate mat (Rotate.pi /. (-2.)))
+      (merge_per_line 0 [])
