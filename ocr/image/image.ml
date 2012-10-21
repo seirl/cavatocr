01364aadef00fb7ae8e3eb20786159db7593931c
Paul Hervot <p_hervot@hotmail.fr>
2012-10-21 03:03:09 +0200

    matrix: move back some functions (strange circular dependency)

--- ocr/image/image.ml
+++ ocr/image/image.ml
@@ -12,8 +12,17 @@ let sdl_init () =
     Sdlevent.enable_events Sdlevent.all_events_mask;
   end
 
-(** Load an image as a SDL rgb surface *)
-let load name = Sdlloader.load_image name
+(** Load an image as a matrix of three color: (int,int,int) array array *)
+let load name =
+  let surface = Sdlloader.load_image name in
+  let (w, h) = get_dims surface in
+  let mat = make w h (0,0,0) in
+    for x = 0 to w - 1 do
+      for y = 0 to h - 1 do
+        mat.(x).(y) <- Sdlvideo.get_pixel_color surface x y
+      done;
+    done;
+    mat
 
 let color_of_bool = function
     | true -> Sdlvideo.black
@@ -44,5 +53,40 @@ let rec wait_key () =
     match e with
         Sdlevent.KEYDOWN _ -> ()
       | _ -> wait_key ()
+
+(** Convert a bool matrix (bool array array) to a black/white SDL surface *)
+let surface_of_matrix matrix =
+  let (w, h) = Matrix.get_dims matrix in
+  let surface = create_surface w h in
+    begin
+      for x = 0 to w - 1 do
+        for y = 0 to h - 1 do
+          Sdlvideo.put_pixel_color surface x y
+            (color_of_bool matrix.(x).(y));
+        done;
+      done;
+      surface
+    end
+
+(** Convert a rgb matrix ((int,int,int) array array) to a SDL surface *)
+let surface_of_rgb_matrix matrix =
+    let (w, h) = Matrix.get_dims matrix in
+    let surface = create_surface w h in
+    begin
+        for x = 0 to w - 1 do
+            for y = 0 to h - 1 do
+                Sdlvideo.put_pixel_color surface x y matrix.(x).(y)
+            done
+        done;
+        surface
+    end
+
+(** Display the corresponding SDL surface of a bool matrix on the dst display *)
+let show_matrix mat dst =
+  show (surface_of_matrix mat) dst
+
+(** Display the corresponding SDL surface of a rgb matrix on the dst display *)
+let show_rgb_matrix mat dst =
+  show (surface_of_rgb_matrix mat) dst
 
 let bool_of_pixel (color,_,_) = color = 255
