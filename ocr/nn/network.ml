3108220a6119199585cce2e1c5b1b65ff2ecc489
serialk <antoine.pietri@epita.fr>
2012-12-04 03:01:39 +0100

    proper layer 1

--- ocr/nn/network.ml
+++ ocr/nn/network.ml
@@ -1,37 +1,48 @@
 let (w_char, h_char) = (16, 10)
 
-class neuron =
+class neuron_layer1 =
 object (self)
-  val weights = Matrix.make w_char h_char 0.0
-
+  val weights = Matrix.make w_char h_char 0
+  val mutable sigma:int = 0
   method get_weight x y = weights.(x).(y)
 
-  method rand_weights =
+  initializer
     for i = 0 to w_char - 1 do
       for j = 0 to h_char - 1 do
-            weights.(i).(j) <- (Random.float 11.) -. 5.
+        weights.(i).(j) <- (Random.int 11) - 5
       done
-    done
+    done;
+    sigma <- (Random.int 11) - 5
 
-  method correct mat error =
+  method inputs mat =
+    let r = ref 0 in
+      for i = 0 to w_char - 1 do
+        for j = 0 to h_char - 1 do
+          r := !r + mat.(i).(j) * weights.(i).(j)
+        done
+      done;
+      !r - sigma
+
+  method output v =
+    if v > 0 then 1 else 0 (* int_of_bool, r u there?? *)
+
+  method eval mat =
+    self#output (self#inputs mat)
+
+  method new_weights mat error =
     for i = 0 to w_char - 1 do
       for j = 0 to h_char - 1 do
         begin
           if mat.(i).(j) = 1 then
-            weights.(i).(j) <- weights.(i).(j) +. error
+            weights.(i).(j) <- weights.(i).(j) + error
         end
       done
     done;
-    count_learn <- count_learn + 1
+    sigma <- sigma - error
 
-  method perceptron mat =
-    let r = ref 0. in
-    for i = 0 to w_char - 1 do
-      for j = 0 to h_char - 1 do
-          r := !r +. mat.(i).(j) *. weights.(i).(j)
-      done
-    done;
-    !r
+  method fix mat expected =
+    let error = expected - (eval mat) in
+    self#new_weights mat error
 end
 
 class network (chars:string) (size:int) =
@@ -39,6 +50,6 @@ object (self)
   val mutable neurons = Hashtbl.create size
   initializer
     for a = 0 to (String.length chars) - 1 do
-      Hashtbl.add neurons chars.[a] (new neuron a)
+      Hashtbl.add neurons chars.[a] (new neuron)
     done
 end
