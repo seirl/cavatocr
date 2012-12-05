756a5cf69fb8d8032be6b849e8e1354f94eb4087
serialk <antoine.pietri@epita.fr>
2012-12-05 01:02:21 +0100

    better documentation, integrated in the website

--- ocr/nn/network.ml
+++ ocr/nn/network.ml
@@ -1,5 +1,9 @@
+(** The neural network module *)
+
+(** These constants are the size of a character matrix *)
 let (w_char, h_char) = (5, 5)
 
+(** A neuron *)
 class neuron =
 object (self)
   val weights = Matrix.make w_char h_char 0
@@ -7,6 +11,7 @@ object (self)
   method get_weight x y = weights.(x).(y)
   method get_weights = weights
 
+  (** Fill the weights with random values *)
   initializer
     for i = 0 to w_char - 1 do
       for j = 0 to h_char - 1 do
@@ -15,6 +20,7 @@ object (self)
     done;
     sigma <- (Random.int 11) - 5
 
+  (** Aggregation function: compute the total input of the neuron *)
   method inputs mat =
     let r = ref 0 in
       for i = 0 to w_char - 1 do
@@ -24,12 +30,15 @@ object (self)
       done;
       !r - sigma
 
+  (** Activation function: compute the output of a neuron *)
   method output v =
     Tools.int_of_bool (v > 0)
 
+  (** Shortcut for the neuron output *)
   method eval mat =
     self#output (self#inputs mat)
 
+  (** Compute the new weights of a neuron *)
   method new_weights mat error =
     for i = 0 to w_char - 1 do
       for j = 0 to h_char - 1 do
@@ -41,11 +50,13 @@ object (self)
     done;
     sigma <- sigma - error
 
+  (** Replace a neuron's weights *)
   method fix mat expected =
     let error = expected - (self#eval mat) in
     self#new_weights mat error
 end
 
+(** The neural network *)
 class network (chars:string) =
 object (self)
   val mutable neurons = Hashtbl.create (String.length chars)
@@ -56,16 +67,19 @@ object (self)
 
   method get_neurons = neurons
 
+  (** Train a neuron with one input *)
   method train_neuron ch mat expected =
     let neuron = Hashtbl.find neurons ch in
       neuron#fix mat expected
 
+  (** Train every neuron of the network with one input *)
   method train_all mat expected_char =
     let iter_train ch neuron =
       self#train_neuron ch mat (Tools.int_of_bool (ch = expected_char))
     in
       Hashtbl.iter iter_train neurons
 
+  (** Recognizes the character in input *)
   method recognize mat =
     let a = ref 0 in
     let found_ch = ref None in
